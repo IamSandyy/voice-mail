2095f9082cb709a588a2f1034752dbd6aec3b697
Santhosh R <136370109+IamSandyy@users.noreply.github.com>
2025-02-04 18:48:16 +0530

    Update CONSTANTS.py

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,3 +1,3 @@
-EMAIL_ID = "santcoff0@gmail.com"   # Put your email id within quotes
-PASSWORD = "uccc ulms zizp nadg"   # Put your password within quotes
+EMAIL_ID = ""   # Put your email id within quotes
+PASSWORD = ""   # Put your password within quotes
 LANGUAGE = "en"
